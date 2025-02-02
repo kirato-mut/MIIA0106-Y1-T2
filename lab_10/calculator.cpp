61d8d2dc67b741b37ad765ddc7678e0f3b64f723
Kiratipat sawangsisombat <capacity1412@gmail.com>
2025-02-02 08:25:28 +0700

    add initial implementation of calculator classes in C++ and Python, along with VSCode configuration files

---
+++ lab_10/calculator.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class Calculator {
+public:
+    int add(int a, int b) {
+        return a + b;
+    }
+};
+
+class SubtractionCalculator : public Calculator {
+public:
+    int subtract(int a, int b) {
+        return a - b;
+    }
+};
+
+class MultiplicationCalculator : public SubtractionCalculator {
+public:
+    int multiply(int a, int b) {
+        return a * b;
+    }
+};
+
+class DivisionCalculator : public MultiplicationCalculator {
+public:
+    int divide(int a, int b) {
+        return a / b;
+    }
+};
+
+int main() {
+    DivisionCalculator mulCalc;
+
+    cout << "Addition (10 + 5): " << mulCalc.add(10, 5) << endl;
+
+    cout << "Subtraction (10 - 5): " << mulCalc.subtract(10, 5) << endl;
+
+    cout << "Multiplication (10 * 5): " << mulCalc.multiply(10, 5) << endl;
+
+    cout << "Division (10 / 5): " << mulCalc.divide(10, 5) << endl;
+
+    return 0;
+}
---
+++ lab_10/calculator.py
@@ -0,0 +1,27 @@
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+class SubtractionCalculator(Calculator):
+    def subtract(self, a, b):
+        return a - b
+
+class MultiplicationCalculator(SubtractionCalculator):
+    def multiply(self, a, b):
+        return a * b
+
+class DivisionCalculator(MultiplicationCalculator):
+    def divide(self, a, b):
+        return a / b
+
+if __name__ == "__main__":
+    mul_calc = DivisionCalculator()
+
+    print(f"Addition (10 + 5): {mul_calc.add(10, 5)}")
+
+    print(f"Subtraction (10 - 5): {mul_calc.subtract(10, 5)}")
+
+    print(f"Multiplication (10 * 5): {mul_calc.multiply(10, 5)}")
+
+    print(f"Division (10 / 5): {mul_calc.divide(10, 5)}")
+
