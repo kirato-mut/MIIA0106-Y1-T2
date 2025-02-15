8108e039a13f192eefcf6812cdcd266876423ea5
Kiratipat sawangsisombat <capacity1412@gmail.com>
2025-02-15 11:30:01 +0700

    add input/output examples in C++ and Python

---
+++ summary1_12/04_input_output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    int age;
+    cout << "Enter name: ";
+    cin >> name;
+    cout << "Enter age: ";
+    cin >> age;
+    cout << "Name: " << name << ", Age: " << age << endl;
+    return 0;
+}
