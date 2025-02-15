7dd442fbea9c1458bdf1da43f49c0e8a9a4491cc
Kiratipat sawangsisombat <capacity1412@gmail.com>
2025-02-15 11:29:54 +0700

    add variable declaration and data type examples in C++ and Python

---
+++ summary1_12/03_variables_and_data_types.py
@@ -0,0 +1,3 @@
+age = 20
+height = 175.5
+print(f"Age: {age}, Height: {height}")
---
+++ summary1_12/03_variables_data_types.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 20;
+    float height = 175.5;
+    cout << "Age: " << age << ", Height: " << height << endl;
+    return 0;
+}
