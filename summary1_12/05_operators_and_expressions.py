c929ccc821de3b0a1825a28e629b5abcfecc9c98
Kiratipat sawangsisombat <capacity1412@gmail.com>
2025-02-15 11:30:08 +0700

    add examples of operators and expressions in C++ and Python

---
+++ summary1_12/05_operators_and_expressions.py
@@ -0,0 +1,3 @@
+radius = 5.0
+area = 3.1416 * radius ** 2
+print(f"Area: {area}")
---
+++ summary1_12/05_operators_expressions.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double radius = 5.0;
+    double area = 3.1416 * radius * radius;
+    cout << "Area: " << area << endl;
+    return 0;
+}
