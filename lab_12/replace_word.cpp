fbe19383e466cc579a31978613b0d304da629103
Kiratipat sawangsisombat <capacity1412@gmail.com>
2025-02-16 09:54:11 +0700

    update data handling in replace_word.cpp and add PDF version of lab document

--- lab_12/replace_word.cpp
+++ lab_12/replace_word.cpp
@@ -7,17 +7,24 @@ using namespace std;
 
 int main() {
     ifstream file("data.txt");
-    if (!file) {
-        cerr << "Unable to open file data.txt";
-        return 1;
-    }
-
     vector<string> lines;
-    string line;
-    while (getline(file, line)) {
-        lines.push_back(line);
+
+    if (!file) {
+        cerr << "Unable to open file data.txt. Creating a new file with default values." << endl;
+        vector<string> default_data = {"Hello", "World", "C++", "first initial"};
+        ofstream outfile("data.txt");
+        for (const auto &l : default_data) {
+            outfile << l << endl;
+        }
+        outfile.close();
+        lines = default_data;
+    } else {
+        string line;
+        while (getline(file, line)) {
+            lines.push_back(line);
+        }
+        file.close();
     }
-    file.close();
 
     string old_word, new_word;
     cout << "Enter word to replace: ";
